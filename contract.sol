a0a0499cf1bea1fb697edc3b8371d6136f77814c
tudor <10128444+tudorelu@users.noreply.github.com>
2019-04-10 22:49:29 +1000

    Updated smart contract
    to contain more info about device

--- contract.sol
+++ contract.sol
@@ -147,7 +147,23 @@ contract Party is Entity {
 /**
  * Defines the ID of an IOT device (a Phone, Sensor, Smart Meter, etc)
  * */
-contract Device is Entity {
-    address         public      adminId;
+ contract Device is Entity {
+    address         public      adminId;        // the address of the device admin (in the case of a hospital, a doctor would own this device and assign it to patients)
+    address         public      userId;         // address of the current user of this device (se this device has write access to this user's data)
     
+    constructor(address payable _uniqueId, address _adminId) Entity (_uniqueId) public {
+        adminId = _adminId;
+    }
+    
+    /*
+        Changes the current user of device 
+        
+        (IE if a patient checks out and another one comes into the same room / bed)
+    */
+    function changeUser(address _newUser){
+        if(msg.sender != adminId)
+            throw;
+            
+        userId = _newUser;
+    }
 }
