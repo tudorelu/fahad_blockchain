ff5be0586be80a5d2eef97545fe9210aedbc92d0
Tudor_Shiba <tudor.barbu7@gmail.com>
2019-04-14 00:04:41 +1000

    db interface & access rights
     Added skeleton functions for reading/writing to db and structure of the access rights . Might consider adding the access rights module to the blockchain

--- Interface/ContractDatabaseInterface.py
+++ Interface/ContractDatabaseInterface.py
@@ -13,6 +13,13 @@ from web3 import Web3, HTTPProvider
 # need to generate the abi derived from the contract itself
 import contract_abi
 
+# this is the database
+import database_file
+
+# this is the file determining access rights (might be placed on the blockchain)
+import access_rights_file
+
+
 ''' 
 	This class defines an interface which enables the interaction between an 
 	Ethereum smart contract and a locally stored SQL database. 
@@ -40,19 +47,22 @@ class ContractDatabaseInterface:
 	def __init__(self):
 
 		# address of contract we'll be interacting with 
-		self.contract_address     = [YOUR CONTRACT ADDRESS]
+		self.contract_address     	= "[YOUR CONTRACT ADDRESS]"
 
 		# private key of user who's interacting with contract
-		self.wallet_private_key   = [YOUR TEST WALLET PRIVATE KEY]
+		self.wallet_private_key   	= "[YOUR TEST WALLET PRIVATE KEY]"
 		
 		# address of user who'se interacting with contract
-		self.wallet_address       = [YOUR WALLET ADDRESS]
+		self.wallet_address			= "[YOUR WALLET ADDRESS]"
 
-		# web3 provider
-		self.w3 = Web3(HTTPProvider([YOUR INFURA URL]))
+		# infura node url
+		self.provider_link			= "kovan.infura.io/v3/3056875747b44d598630b4975043e928";
+		
+		# infura node (web3 provider)
+		self.w3 					= Web3(HTTPProvider(self.provider_link))
 		
 		# contract that the user will be interacting with
-		self.contract = self.w3.eth.contract(address = self.contract_address, abi = contract_abi.abi)
+		self.contract 				= self.w3.eth.contract(address = self.contract_address, abi = contract_abi.abi)
 
 		# enables unaudited features of web3
 		#self.w3.eth.enable_unaudited_features()
@@ -60,7 +70,7 @@ class ContractDatabaseInterface:
 	def send_ether_to_contract(self, amount_in_ether):
 
 		# amount (in wei) to send to the contract, derived from ether amount (10^18 wei = 1 ether)
-	    amount_in_wei = self.w3.toWei(amount_in_ether,'ether');
+	    amount_in_wei = self.w3.toWei(amount_in_ether, 'ether');
 
 	    # generate nonce (tx c)
 	    nonce = self.w3.eth.getTransactionCount(self.wallet_address)
@@ -72,7 +82,7 @@ class ContractDatabaseInterface:
 	            'gas': 2000000,
 	            'gasPrice': w3.toWei('40', 'gwei'),
 	            'nonce': nonce,
-	            'chainId': 3		# chainId = 1 for main net, 3 for ropsten, 42 for kovan; 
+	            'chainId': 42		# chainId = 1 for main net, 3 for ropsten, 42 for kovan; 
 	            					# check https://github.com/ethereumbook/ethereumbook/issues/110
 	    }
 
@@ -94,9 +104,30 @@ class ContractDatabaseInterface:
 	    # if it hasn't been received within 300 seconds, it means the transaction failed
 	    if txn_receipt is None:
 	        return {'status': 'failed', 'error': 'timeout'}
+
 	    # otherwise, it was added
 	    return {'status': 'added', 'txn_receipt': txn_receipt}
 
 	'''	this is how a contract function call looks like	'''
     def call_contract_function(self, function_name, params):
 	    return self.contract.functions.function_name(params).call()
+
+	def read_from_database(self, address_of_reader, address_of_data_owner, path_to_read):
+		
+		# Check access rights of reader 
+		if access_rights_file[address_of_data_owner][address_of_reader]["read"].contains(path_to_read):
+			# Display data
+			print(database_file[address_of_data_owner][path_to_read])
+		else:
+			print(" Access denied. ")
+
+
+	def write_to_database(self, address_of_writer, address_of_data_owner, path_to_write_to, data):
+		
+		# Check access rights of reader 
+		if access_rights_file[address_of_data_owner][address_of_writer]["write"].contains(path_to_write_to):
+			# Write data
+			database_file[address_of_data_owner][path_to_write_to] = data
+			print(" Wrote "+data+" to "+path_to_write_to+" iside "+address_of_data_owner+"\'s data ")
+		else:
+			print(" Access denied. ")
