e449d8f7be9276cadd758e3990197fae96efba67
tudor <10128444+tudorelu@users.noreply.github.com>
2019-04-17 13:30:24 +1000

    Updated main contract
    Added entities as structs and placed everything in one contract. Will try to run with everything as structs instead of contracts due to lower deployment costs and better memory management.

--- Contracts/contract.sol
+++ Contracts/contract.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity >=0.4.22 <0.6.0;
 
     /**
      * This contract defines the interactions between three entities - agent, 
@@ -26,11 +26,85 @@ pragma solidity ^0.5.0;
 /**
  * Defines the main contract which allows the interaction between devices, agents and parties
  * */
- contract Interaction { 
 
-    address         admin;
+//import './Organization.sol';
 
-    constructor() {
+contract System {
+    
+    address payable admin;
+    
+    mapping(address => bool) orgs;
+    mapping(address => bool) agents;
+    
+    struct Entity {
+        address payable uniqueId;                   // used as a unique id
+        int             tier;                       // determines the access rights
+    }
+    
+    struct Agent {
+        Entity          ent;
+        address         orgId;                      // id of organization this Agent belongs
+    }
+    
+    struct Organization {
+        Entity          ent;
+        address         adminId;                    // address of Organization admin
+        int             maxAgentTier;               // max tier of Organization members
+    }
+    
+    struct Device {
+        Entity          ent;
+    }
+    /* The list of agents that belong to each organization.
+        First address is the uniqueId of an organization;
+        Second address is the uniqueId of an Agent;
+    */
+    mapping(address => mapping(address => Agent)) organizationAgents;
+    mapping(address => mapping(address => Organization)) organizationOrgs;
+    
+    constructor () public {
         admin = msg.sender;
     }
- }
+    
+    /*  Validates address as being of an Agent's by adding it to list of valid Agents. */
+    function createNewAgent (address payable _agentId) public {
+        agents[_agentId] = true;
+    }
+    
+    /* Check whether address belonngs to a valid Agent */
+    function isAgent (address payable _agentId) public view returns (bool) {
+        return agents[_agentId];
+    }
+    
+    /* Revokes the address' validity as being of an agent's by removing it from list of Agents. */
+    function removeAgent (address payable _agentId) public {
+        agents[_agentId] = false;
+    }        
+    
+    /*  Validates address as being of an Organization by adding it to list of valid Orgs. */
+    function createNewOrganization (address payable _orgId) public {
+        orgs[_orgId] = true;
+    }
+    
+    /* Check whether address belonngs to a valid Organization */
+    function isOrg (address payable _orgId) public view returns (bool) {
+        return orgs[_orgId];
+    }        
+
+    /* Revokes the address' validity as being of an organization's by removing it from list of Orgs. */
+    function removeOrganization (address payable _orgId) public {
+        orgs[_orgId] = false;
+    }
+    
+    function assignAgentToOrganization (address payable _agentId, address payable _orgId) public {
+        require(agents[_agentId]);
+        
+        Agent memory agent;
+        agent.ent.uniqueId = _agentId;
+        agent.orgId = _orgId;
+
+        organizationAgents[_orgId][_agentId] = agent;
+        
+    }
+    
+}
