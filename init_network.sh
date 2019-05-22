705b0dc8fe1cbac1f1782766b1cd83b98e379263
Tudor (Ucil Mac) <tudor.barbu7@gmail.com>
2019-05-22 20:49:00 +1000

    Removed Blockchain data from git

--- init_network.sh
+++
@@ -1,5 +0,0 @@
-
-geth --identity 898911 init Private\ Blockchain/genesis.json --datadir Private\ Blockchain/ChainData
-
-geth --datadir Private\ Blockchain/ChainData --networkid 898911 --rpc --rpcport 8543 --rpcaddr 127.0.0.1 --rpccorsdomain "*" --rpcapi "eth,net,web3,personal,miner" --nodiscover --port 0 console
-
